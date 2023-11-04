43e7cad6225a6ec174343d46f1491d66de2bf808
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-04 15:37:07 +0100

    add gradient to Home.jsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,24 @@
+// import test from "/src/assets/test.png";
+
+export const Home = () => {
+  return (
+    <section className="relative flex h-[600px] w-full">
+      <div className="flex h-[600px] w-full">
+        <div className="custom-bg-gradient2 w-1/3"></div>
+        <div className="custom-bg-gradient w-2/3"></div>
+      </div>
+      <div className="absolute left-0 top-0">
+        <h1 className="text-white">Artur</h1>
+      </div>
+    </section>
+  );
+};
+
+// <div>
+//           <h4>Hi There! I&rsquo;m</h4>
+//           <h1>Artur</h1>
+//           <h3>Frontend developer</h3>
+//         </div>
+//         <div>
+//           <img src={test} alt="test" />
+//         </div>
