7f3331e68be280e2e57f93107be2b2af5ca117df
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-06 13:38:30 +0100

    add propsType to mainButton.jsx

--- src/elements/MainButton.tsx
+++ src/elements/MainButton.tsx
@@ -1,4 +1,8 @@
-export const MainButton = ({ children }) => {
+type ButtonProps = {
+  children: React.ReactNode;
+};
+
+export const MainButton = ({ children }: ButtonProps) => {
   return (
     <button className="bg-gradient-linear hover:shadow-gradient-linear-shadow rounded-md px-6 py-3 font-bold uppercase tracking-widest duration-100 ease-out hover:scale-110 hover:bg-white hover:bg-none hover:text-black">
       {children}
