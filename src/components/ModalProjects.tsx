2a5b5ef41449632ec0dc3cb322789cae34a9e261
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-08 18:33:32 +0100

    reorganization and add logic to ModalProjects.tsx

--- src/components/ModalProjects.tsx
+++ src/components/ModalProjects.tsx
@@ -1,4 +1,5 @@
 import { MainButton } from "../elements/MainButton";
+import { useState } from "react";
 
 type ModalProps = {
   isOpenModal: boolean;
@@ -6,6 +7,10 @@ type ModalProps = {
   item: {
     id: number;
     title: string;
+    description: string;
+    features: { feature: string }[];
+    technologies: { technology: string }[];
+    gallery: { image: string }[];
   };
 };
 
@@ -14,50 +19,75 @@ export const ModalProjects = ({
   setIsOpenModal,
   item,
 }: ModalProps) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setSelectedImage(null);
+  };
+
   return (
-    <div
-      onClick={() => setIsOpenModal(!isOpenModal)}
-      className="fixed left-0 top-0 z-10 h-screen w-full bg-transparent"
-    >
+    <>
       <div
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        className="bg-darknes-bg fixed inset-0 left-1/2 top-1/2 z-20 h-5/6 w-3/4 -translate-x-1/2 -translate-y-1/2  rounded-md px-8 py-4 backdrop-blur-xl"
+        onClick={() => setIsOpenModal(!isOpenModal)}
+        className="fixed left-0 top-0 z-10 h-screen w-full bg-transparent"
       >
-        <h2 className="border-b-[1px] border-stone-400 pb-8 pt-4 text-4xl font-bold">
-          {item.title}
-        </h2>
-        <h3 className="mb-4 mt-8 text-xl">About the project</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum tempore
-          dolores atque necessitatibus explicabo iusto delectus eligendi nihil
-          veritatis, porro soluta laboriosam asperiores, dolorum molestias
-          itaque voluptas! In, eveniet deleniti! Lorem, ipsum dolor sit amet
-          consectetur adipisicing elit. In incidunt aspernatur vero adipisci
-          assumenda! Harum tempore totam id modi eligendi ex nesciunt earum
-          tenetur? Inventore quisquam nam quae officia esse?
-        </p>
-        <h3 className="mb-4 mt-8 text-xl">Features</h3>
-        <ul>
-          <li>Dodawanie wiadomo</li>
-          <li>Usuwanie tez mozliwe</li>
-          <li>Mozna zmienic ilosc</li>
-          <li>eleganckie podsumowanie na koniec jest</li>
-        </ul>
-        <h3 className="mb-4 mt-8 text-xl">Technologies and tools</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Redux</li>
-          <li>React Router</li>
-          <li>Tailwind</li>
-        </ul>
-        <div className="mt-8 space-x-10">
-          <MainButton>Repo</MainButton>
-          <MainButton>Website</MainButton>
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className="bg-darknes-bg fixed inset-0 left-1/2 top-1/2 z-20 h-5/6 w-3/4 -translate-x-1/2 -translate-y-1/2  overflow-y-auto rounded-md px-8 py-4 backdrop-blur-xl"
+        >
+          <h2 className="border-b-[1px] border-stone-400 pb-8 pt-4 text-4xl font-bold">
+            {item.title}
+          </h2>
+          <div className="mt-4 flex gap-x-4">
+            {item.gallery.map((gallery, i) => (
+              <div key={i} className="w">
+                <img
+                  src={gallery.image}
+                  alt={gallery.image}
+                  onClick={() => handleImageClick(gallery.image)}
+                />
+              </div>
+            ))}
+          </div>
+          <h3 className="mb-4 mt-8 text-xl">About the project</h3>
+          <p>{item.description}</p>
+          <h3 className="mb-4 mt-8 text-xl">Features</h3>
+          <ul>
+            {item.features.map((features, i) => (
+              <li key={i}>{features.feature}</li>
+            ))}
+          </ul>
+          <h3 className="mb-4 mt-8 text-xl">Technologies and tools</h3>
+          <ul>
+            {item.technologies.map((technologies, i) => (
+              <li key={i}>{technologies.technology}</li>
+            ))}
+          </ul>
+          <div className="mt-8 space-x-10">
+            <MainButton>Repo</MainButton>
+            <MainButton>Website</MainButton>
+          </div>
         </div>
       </div>
-    </div>
+      {modalVisible && (
+        <div
+          onClick={() => setModalVisible(!modalVisible)}
+          className="bg-darknes-bg fixed left-0 top-0 z-30 h-full w-full backdrop-blur-xl"
+        >
+          <div className="fixed left-1/2 top-1/2 z-40 h-5/6 w-5/6 -translate-x-1/2 -translate-y-1/2">
+            <img src={selectedImage} alt={selectedImage} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
