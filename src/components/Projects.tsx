e5266ebbe90add548fa78e6630ce27477c7c5344
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-08 20:21:55 +0100

    change first and second project data i json

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import data from "../data.json";
 
 export const Projects = () => {
   return (
-    <section className="bg-[#14071A] py-5 text-white">
+    <section className="bg-[#14071A] py-8 text-white">
       <h2 className="my-10 text-center text-4xl">Projects</h2>
       <div className="mx-auto flex max-w-7xl gap-x-6">
         {data.map((item) => (
