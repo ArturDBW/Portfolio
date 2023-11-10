5702db62fc9bb5cdff9963af566a02c1b5bfc796
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-10 15:45:27 +0100

    add structure to infoContact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,5 @@
-import { Mailsvg } from "../svg/Mailsvg";
-import { Phonesvg } from "../svg/Phonesvg";
-import { Pinsvg } from "../svg/Pinsvg";
 import { FormContact } from "./FormContact";
+import { InfoContact } from "./InfoContact";
 
 export const Contact = () => {
   return (
@@ -9,14 +7,7 @@ export const Contact = () => {
       <h2 className="my-10 text-center text-4xl">Contact</h2>
       <div className="mx-auto flex max-w-7xl gap-x-6">
         <FormContact />
-        <div className="w-1/2 rounded-md bg-white bg-opacity-10 p-4">
-          <div className="flex">
-            <Mailsvg />
-            <p className="ml-4">Arturwisniewski1998@gmail.com</p>
-          </div>
-          <Pinsvg />
-          <Phonesvg />
-        </div>
+        <InfoContact />
       </div>
     </div>
   );
--- src/components/InfoContact.tsx
+++ src/components/InfoContact.tsx
@@ -1,3 +1,28 @@
+import { GitHubsvg } from "../svg/GitHubsvg";
+import { Linkedlnsvg } from "../svg/Linkedlnsvg";
+import { Mailsvg } from "../svg/Mailsvg";
+import { Phonesvg } from "../svg/Phonesvg";
+import { Pinsvg } from "../svg/Pinsvg";
+
 export const InfoContact = () => {
-  return <div>InfoContact</div>;
+  return (
+    <div className="w-1/2 space-y-4 rounded-md bg-white bg-opacity-10 p-4">
+      <div className="flex">
+        <Mailsvg />
+        <span className="ml-5">Arturwisniewski1998@gmail.com</span>
+      </div>
+      <div className="flex">
+        <Pinsvg />
+        <span className="ml-5">77-100 Bytów, Poland</span>
+      </div>
+      <div className="flex">
+        <Phonesvg />
+        <span className="ml-5">+48 889 737 046</span>
+      </div>
+      <div>
+        <GitHubsvg />
+        <Linkedlnsvg />
+      </div>
+    </div>
+  );
 };
