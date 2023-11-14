4e94a77f1b7080a8f286e97d46bcaa3362c9e4bd
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-14 10:18:43 +0100

    delate navigation

--- src/components/InfoContact.tsx
+++ src/components/InfoContact.tsx
@@ -8,8 +8,11 @@ export const InfoContact = () => {
   return (
     <div className="flex w-1/2 flex-col items-start justify-between space-y-3 rounded-md bg-white bg-opacity-10 p-4">
       <div>
-        <h3 className="mb-4 text-3xl">Lets talk!</h3>
-        <p className="text mb-6 mt-2">
+        <h3 className="mb-4 text-3xl ">
+          <span className="gradient-text mr-1 font-bold">Let&rsquo;s</span>
+          talk!
+        </h3>
+        <p className="text mb-6 mt-2 text-lg">
           Thank you for visiting my portfolio. If you are interested in
           cooperation, please contact me!
         </p>
