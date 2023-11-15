c95042d4f49a3f2f8d39629cf81dd5a52177b357
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-15 11:20:17 +0100

    changes styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="bg-[#07010e] py-10 text-white">
       <ul className="flex justify-center space-x-4">
         <li>About</li>
-        <li>GitHub</li>
+        <li className="border-l border-r px-4">GitHub</li>
         <li>Linkedln</li>
       </ul>
     </footer>
