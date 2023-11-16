bf2c72648092868bfb14e9b31e35051f3327bb27
Artur Wiśniewski <artixbtw@gmail.com>
2023-11-16 20:34:47 +0100

    add styles to Footer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import { InfoContact } from "./InfoContact";
 
 export const Contact = () => {
   return (
-    <div className="border-b-[1px] border-stone-700 px-10 py-8 dark:bg-[#14071A] dark:text-white max-sm:px-5">
+    <div className="px-10 py-8 dark:bg-[#14071A] dark:text-white max-sm:px-5">
       <h2 className="my-10 text-center text-4xl">Contact</h2>
       <div className="mx-auto flex max-w-7xl gap-x-6 max-md:flex-col-reverse max-md:gap-y-10 max-sm:gap-y-5">
         <FormContact />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,23 @@
 export const Footer = () => {
+  const liStyles = "gradient-text-hover font-bold duration-150";
   return (
-    <footer className="bg-[#07010e] py-10 text-white">
+    <footer className="bg-[#f7efef] py-10 text-lg dark:bg-[#07010e] dark:text-white">
       <ul className="flex justify-center space-x-4">
-        <li>About</li>
-        <li className="border-l border-r px-4">GitHub</li>
-        <li>Linkedln</li>
+        <li className={liStyles}>About</li>
+        <a href="https://github.com/ArturDBW" target="_blank" rel="noreferrer">
+          <li
+            className={`${liStyles} border-l border-r border-black px-4 dark:border-white`}
+          >
+            GitHub
+          </li>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/artur-wi%C5%9Bniewski-4045041b8/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <li className={liStyles}>Linkedln</li>
+        </a>
       </ul>
     </footer>
   );
