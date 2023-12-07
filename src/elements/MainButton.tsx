8756b423db174c6bbe6be23f1ee7093f04384127
Artur Wiśniewski <artixbtw@gmail.com>
2023-12-07 14:23:48 +0100

    delete dispaly block from button

--- src/elements/MainButton.tsx
+++ src/elements/MainButton.tsx
@@ -7,7 +7,7 @@ export const MainButton = ({ children, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="block rounded-lg bg-gradient-to-r from-[#1D2B64] to-[#F8CDDA] bg-cover px-10 py-4 text-center uppercase text-white shadow-lg transition duration-500 hover:bg-gradient-to-l dark:from-[#AA076B] dark:to-[#61045F]"
+      className="rounded-lg bg-gradient-to-r from-[#1D2B64] to-[#F8CDDA] bg-cover px-10 py-4 text-center uppercase text-white shadow-lg transition duration-500 hover:bg-gradient-to-l dark:from-[#AA076B] dark:to-[#61045F]"
     >
       {children}
     </button>
